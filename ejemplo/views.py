358a166e26d9cf0dbef1aaa7cf8c5d9f5660e12c
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2022-10-13 20:54:29 -0300

    agrego una lista con for

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", 
+    {
+        "nombre":"Ezequiel",
+        "apellido":"Bosco",
+        })
+
+def index_1(request):
+    return render(request, "ejemplo/saludar.html",
+    {"notas": [1,2,3,4,5]}
+    )
+
