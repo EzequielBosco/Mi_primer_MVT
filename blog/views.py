89e7d5b98f63896546531e3a217d1947e34a9fac
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2022-10-26 21:07:46 -0300

    agrego models

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from blog.models import Configuracion
 
 def index_0(request):
-    return render(request, "blog/index.html")
+    configuracion = Configuracion.objects.first()
+    return render(request, "blog/index.html", {"configuracion": configuracion})
